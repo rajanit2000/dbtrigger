c14b2252dded8c76136946b6b12b305e3ef0a693
Rajan <rajanit2000@gmail.com>
2016-08-29 18:25:56 +0530

    Update db-trigger-set.php

--- includes/db-trigger-set.php
+++ includes/db-trigger-set.php
@@ -1,13 +1,17 @@
-<?php
-settings_fields( 'trigger-db-settings-group' );
-do_settings_sections( 'trigger-db-settings-group' );
+<div class="wrap">
+	<h1>Set Trigger</h1>	
+	<br/>
+	<?php
+	settings_fields( 'trigger-db-settings-group' );
+	do_settings_sections( 'trigger-db-settings-group' );
 
-if( get_option('tdb_mode') == 'false' ){
-	echo '	<input
-				type	= "button" 
-				value	= "Create Trigger" 
-				class	= "button button-primary" 
-			/>';
-}else{
-	echo "Already Created";
-}
+	if( get_option('tdb_mode') == 'false' ){
+		echo '	<input
+					type	= "button" 
+					value	= "Create Trigger" 
+					class	= "button button-primary" 
+				/>';
+	}else{
+		echo "Already Created";
+	}?>
+</div>
